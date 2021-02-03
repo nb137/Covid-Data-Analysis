7218f96f2de4cee3e602d66d47847c8a55da14f7
nb137 <nb137@users.noreply.github.com>
2021-02-03 13:07:53 -0800

    Update 2/3
    Data updated for 2/3
    Table of % total and current added to Covid Data Plots

--- _Vaccine_Data_Review.py
+++ _Vaccine_Data_Review.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=pd.core.common.SettingWithCopyWarning)
+
 
 from covid_support import load_vaccine_data, state_info
 vf = load_vaccine_data()
@@ -33,7 +35,7 @@ shortlist_states = state_info.loc[[1,9,12,14,15]]  # CA NY OR TX WA
 # Total Vaccines vs. Time
 # TODO maybe use interpolatin instead of ffill na, but maybe that it is bad to infer any data
 plt.figure(figsize=(12,6))
-for i,r in state_info.iterrows():
+for i,r in shortlist_states.iterrows():
     try:
         vac_per_pop = (vf['people_total',r['State']]/(r['Pop']/1e2)).fillna(method='ffill')
         second_vac_per_pop = (vf['people_total_2nd_dose',r['State']]/(r['Pop']/1e2)).fillna(method='ffill')
@@ -49,7 +51,7 @@ plt.grid(which='both')
 
 
 plt.figure(figsize=(12,6))
-for i,r in state_info.iterrows():
+for i,r in shortlist_states.iterrows():
     try:
         dose_per_pop = (vf['doses_admin_total',r['State']]/(r['Pop']/1e2)).fillna(method='ffill')
         plt.plot(vf.index, dose_per_pop,linewidth=3, label=r['Short'],linestyle='solid',color=r['c'])
@@ -64,7 +66,7 @@ plt.grid(which='both')
 
 # New Shots per day normalized
 plt.figure(figsize=(12,6))
-for i,r in state_info.iterrows():
+for i,r in shortlist_states.iterrows():
     new_shots_per_pop = (vf['doses_admin_total',r['State']]/(r['Pop']/1e2)).diff().rolling(7).mean().fillna(0)
     plt.scatter(vf.index, (vf['doses_admin_total',r['State']]/(r['Pop']/1e2)).diff(),color=r['c'],s=7,alpha=0.5,label=r['State']+' raw')
     plt.plot(vf.index, new_shots_per_pop,linewidth=3, label=r['State']+' 7d avg',color=r['c'])
